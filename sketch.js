b0f47c3fc7171f9be12c8cd765ba211f5ab81c29
maria paula <mp.vargast79@gmail.com>
2019-12-11 00:51:29 +0100

    deliver

--- sketch.js
+++ sketch.js
@@ -1,11 +1,48 @@
-function preload(){
-  // put preload code here
+var lego;
+var face;
+var song;
+
+
+function preload() {
+  lego = loadModel("assets/lego.obj", true);
+  face = loadImage("assets/Face_04.png");
+  song = loadSound("assets/Naked.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  noStroke();
+  song.play();
+
 }
 
 function draw() {
-  // put drawing code here
+  background(20);
+
+  camera(mouseX - width / 2, mouseY - height / 2, -300, 2, 0, 0, 0, 1, 0);
+
+  let locX = mouseX - width / 2;
+  let locY = mouseY - height / 2;
+
+  pointLight(207, 255, 249, locX, locY, 50);
+
+  // Blue directional light from the left
+  directionalLight(178, 0, 209, 0, 1, 0);
+  directionalLight(0, 255, 225, 0, 0, -1);
+  directionalLight(random(255), 102, 255, -1, 0, 0);
+
+  ambientLight(59, 44, 1);
+
+  texture(face);
+  rotateZ(180);
+  rotateY(locY * -0.5);
+  rotateX(map(mouseY, 0, height, 0, PI));
+  model(lego);
+
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
